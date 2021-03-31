dccf6e48f7e3e5a781cc72dd2ff98861171f3043
jacker <jacker@healthcheck-ui.local127.0.1.1>
2021-03-31 09:48:32 -0300

    edit proxy and server base url

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -7,7 +7,7 @@ import { ApolloProvider } from "@apollo/react-hooks";
 import { setContext } from "apollo-link-context";
 
 const httpLink = new createHttpLink({
-  uri: "http://localhost:5000",
+  uri: "https://cryptic-oasis-19573.herokuapp.com/",
 });
 
 const authLink = setContext(() => {
